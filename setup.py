d73b58636238d94ac8dd73bf920dcb619e257121
EkaterinaLevashova <voxquietam.md@gmail.com>
2024-05-24 10:32:39 +0300

    feat(setup): add setup props

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 __version__ = "2.0.1"
 
 setup(
     name="request-api-builder",
     version=__version__,
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
 )
