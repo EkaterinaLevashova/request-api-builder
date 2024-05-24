e211562a7c39a4eb9639353066435b8e5cb019c7
EkaterinaLevashova <voxquietam.md@gmail.com>
2024-05-24 11:01:25 +0300

    feat(lib): update setup name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 __version__ = "2.0.2"
 
 setup(
-    name="request-api-builder",
+    name="request_api_builder",
     version=__version__,
     packages=find_packages(),
     install_requires=[
